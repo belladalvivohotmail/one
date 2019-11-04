b87db58624f9e3a936a8822aea8053804d5d9f1b
belladalvivohotmail <41295369+belladalvivohotmail@users.noreply.github.com>
2019-11-04 16:55:23 +0100

    Update request.php

--- request.php
+++ request.php
@@ -1,1 +1,33 @@
 
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Excel download</title>
+        <link rel="icon"  href="favicon.ico" />
+
+   </head>
+   
+<body>
+
+    <div class="pull-right">
+                <p style=" font-weight: bold; float: left; padding-right: 10px;"> Excel file</p>
+                <button type="submit" onclick="location.href='answer.php'" class="btn btn-xs btn-primary" id="command-telecharger" data-row-id="0"><span class="glyphicon glyphicon-plus"></span> Donwload </button>
+
+            
+            
+
+           
+                
+        </div>
+    
+               
+    
+</body>
+
+</html>
